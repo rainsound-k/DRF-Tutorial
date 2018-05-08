a900bf67aabd89d3492f19e50cdc810e856e0f79
rainsound-k <rainsound128@gmail.com>
2018-05-08 14:05:45 +0900

    SnippetDetail 뷰 구현

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('snippets.urls')),
+    path('snippets/', include('snippets.urls')),
 ]
--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+app_name = 'snippets'
+
+urlpatterns = [
+    path('', views.SnippetList.as_view()),
+    path('<int:pk>/', views.SnippetDetail.as_view()),
 ]
